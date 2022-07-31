00b44814fac537d137a39947f6fefa2448f7c3c7
apolloiheo <apolloiheo@berkeley.edu>
2022-07-31 05:48:45 -0700

    created chatrooms

--- src/goblingame/data/front-end/src/js/index.js
+++ src/goblingame/data/front-end/src/js/index.js
@@ -1,20 +1,37 @@
 // empty
 
 /* Web Socket */
-let socket = new WebSocket("ws://localhost:8000/ws");
-socket.onopen = (event) => {
+let websocket = new WebSocket("ws://localhost:8000/ws");
+websocket.onopen = (event) => {
     alert("[Connection Established]");
+    CreateMessage("message", ["Connection Established"], []);
 };
-socket.onmessage = (event) => {
+websocket.onmessage = (event) => {
     if (event.data == "connected") return;
     console.log(event.data);
     let event_data = JSON.parse(event.data);
-    console.log(event_data);
-    if (event_data.source != "s") return;
     
-    if (event_data.type == "ws_setup") {
-        window.uuid = event_data.content;
+    if (event_data.source == "c") {
+        if (event_data.type == "ws-setup") {
+            window.uuid = event_data.content;
+            CreateMessage("message", [], [`UUID: ${uuid}`]);
+        }
+        
+        if (event_data.type == "room-join-s") {
+            CreateMessage("message", [`Room ${event_data.content} Joined`], []);
+        } if (event_data.type == "room-join-f") {
+            CreateMessage("message", [`Could not join Room ${event_data.content}`], []);
+        }
+    } else {
+        if (event_data.type == "room-message") {
+            CreateMessage(
+                "message",
+                [],
+                [`>${event_data.source} > ${event_data.content}`]
+            )
+        }
     }
+    
 };
 
 /* Input Button */
@@ -26,38 +43,59 @@ text_input.addEventListener("keyup", function(event) {
 })
 
 function InputEnter() {
-    console.log(text_input.value);
-    CreateMessage("user", text_input.value);
-    socket.send(text_input.value);
+    // test
+    if (text_input.value == "CREATE ROOM") {
+        send_message = {
+            "source": uuid,
+            "type": "room-create",
+            "content": ""
+        };
+        websocket.send(JSON.stringify(send_message));
+        text_input.value = "";
+        return;
+    } if (text_input.value.slice(0, 4) == "JOIN") {
+        send_message = {
+            "source": uuid,
+            "type": "room-join",
+            "content": text_input.value.slice(5, text_input.value.length)
+        }
+        websocket.send(JSON.stringify(send_message));
+        text_input.value = "";
+        return;
+    }
+
+    CreateMessage("message", [], [text_input.value]);
+    websocket.send(JSON.stringify({
+        "source": uuid,
+        "type": "room-message",
+        "content": text_input.value
+    }));
     text_input.value = "";
 }
 
-function CreateMessage(from, content) {
+function CreateMessage(type, header, content) {
     let messages = document.getElementsByClassName("messages")[0];
-    if (from == "user") {
-        messages.innerHTML +=
-            "<div class=\"message\"> \
-                <p class=\"message-content\">" + content + "</p> \
-            </div>";
-    } else if (from == "server") {
-        // content = [
-        //      type: message,
-        //      header: [string],
-        //      content: [string]
-        // ]
-        if (content[0] == "message") {
-            headings = "";
-            for (let i in content[1]) {
+    // content = [
+    //      type: message,
+    //      header: [string],
+    //      content: [string]
+    // ]
+    if (type == "message") {
+        headings = "";
+        if (header.length > 0) {
+            for (let i of header) {
                 headings += "<p class=\"message-heading\">" + i + "</p>";
             }
-            contents = "";
-            for (let i in content[2]) {
+        }
+        contents = "";
+        if (content.length > 0) {
+            for (let i of content) {
                 contents += "<p class=\"message-content\">" + i + "</p>";
             }
-            messages.innerHTML +=
-            "<div class=\"message\">"
-                + headings + contents
-            "</div>";
         }
+        messages.innerHTML +=
+        "<div class=\"message\">"
+            + headings + contents
+        "</div>";
     }
 }
--- src/goblingame/server.py
+++ src/goblingame/server.py
@@ -6,6 +6,8 @@ from fastapi.responses import HTMLResponse
 
 from goblingame.data import DATA
 
+import json, random
+
 app = FastAPI()
 
 origins = [
@@ -29,19 +31,29 @@ async def get():
     return HTMLResponse(index_html)
 
 # temp database
-import json
 ws_conns = {
     # str(host:port): {data}
 }
 
 uuids = []
 def uuid_gen():
-    import random
     while True:
         x = int(random.random() * 1e6)
         if x not in uuids:
             break
     uuids.append(x)
+    return x
+
+rooms = {}
+room_chars = [str(i) for i in range(10)] \
+    + [chr(i) for i in range(ord('a'), ord('z')+1)] \
+    + [chr(i) for i in range(ord('A'), ord('Z')+1)]
+def room_gen():
+    while True:
+        x = "".join(random.choice(room_chars) for _ in range(3))
+        if x not in rooms:
+            break
+    rooms[x] = {}
     return x
 
 @app.websocket("/ws")
@@ -50,15 +62,16 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     client = websocket.client
     client_str = f"{client.host}:{client.port}"
+    uuid = uuid_gen()
     ws_conns[f"{client.host}:{client.port}"] = {
         "websocket": websocket,
-        "uuid": uuid_gen(),
+        "uuid": uuid,
     }
 
     # send uuid info
     send_data = {
-        "source": "s",
-        "type": "ws_setup",
+        "source": "c",
+        "type": "ws-setup",
         "content": ws_conns[client_str]["uuid"]
     }
     await websocket.send_text(json.dumps(send_data))
@@ -67,10 +80,59 @@ async def websocket_endpoint(websocket: WebSocket):
         try:
             data = await websocket.receive_text()
             data = json.loads(data)
+
+            if data["type"] == 'room-create':
+                room_id = room_gen()
+                ws_conns[client_str]['room'] = room_id
+                rooms[room_id] = {
+                    "users": [client_str]
+                }
+                send_message = {
+                    "source": "c",
+                    "type": "room-join-s",
+                    "content": room_id
+                }
+                await websocket.send_text(json.dumps(send_message))
+            if data["type"] == 'room-join':
+                room_id = data['content']
+                if room_id in rooms:
+                    if 'room' in ws_conns[client_str]:
+                        rooms[room_id].remove(ws_conns[client_str]['room'])
+                    ws_conns[client_str]['room'] = room_id
+                    rooms[room_id]["users"].append(client_str)
+                    send_message = {
+                        "source": "c",
+                        "type": "room-join-s",
+                        "content": room_id
+                    }
+                else:
+                    send_message = {
+                        "source": "c",
+                        "type": "room-join-f",
+                        "content": room_id
+                    }
+                await websocket.send_text(json.dumps(send_message))
+            
+            # share message with everyone in room
+            if data['type'] == 'room-message':
+                send_message = {
+                    "source": uuid,
+                    "type": "room-message",
+                    "content": data["content"]
+                }
+                if 'room' in ws_conns[client_str]:
+                    room_id = ws_conns[client_str]['room']
+                    if room_id in rooms:
+                        for i in rooms[room_id]['users']:
+                            if i == client_str:
+                                continue
+                            temp_ws = ws_conns[i]['websocket']
+                            await temp_ws.send_text(json.dumps(send_message))
         except ws.WebSocketDisconnect:
             ws_conns[client_str]
+            # code form disconnecting person
             break
-        await websocket.send_text(f"Message received: {data}")
+        # await websocket.send_text(f"Message received: {data}")
 
 @app.get("/src/css/main.css")
 async def get():
@@ -78,7 +140,6 @@ async def get():
         DATA.joinpath("front-end/src/css/main.css").read_text(),
         media_type="text/css"
     )
-    
 @app.get("/src/js/index.js")
 async def get():
     return HTMLResponse(
