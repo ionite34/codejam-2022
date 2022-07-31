68c42281f5e0779d3f4291e129c20cdcd7cd3340
apolloiheo <apolloiheo@berkeley.edu>
2022-07-30 19:39:27 -0700

    more js for ws

--- front-end/src/js/index.js
+++ front-end/src/js/index.js
@@ -1,11 +1,16 @@
 // empty
 
+/* Web Socket */
+
+
+/* Input Button */
 let text_input = document.getElementById("user-message");
 text_input.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
         InputEnter();
     }
 })
+
 function InputEnter() {
     console.log(text_input.value);
     CreateMessage("user", text_input.value);
@@ -13,11 +18,31 @@ function InputEnter() {
 }
 
 function CreateMessage(from, content) {
+    let messages = document.getElementsByClassName("messages")[0];
     if (from == "user") {
-        let messages = document.getElementsByClassName("messages")[0];
         messages.innerHTML += 
             "<div class=\"message\"> \
                 <p class=\"message-content\">" + content + "</p> \
             </div>";
+    } else if (from == "server") {
+        // content = [
+        //      type: message,
+        //      header: [string],
+        //      content: [string]
+        // ]
+        if (content[0] == "message") {
+            headings = "";
+            for (let i in content[1]) {
+                headings += "<p class=\"message-heading\">" + i + "</p>";
+            }
+            contents = "";
+            for (let i in content[2]) {
+                contents += "<p class=\"message-content\">" + i + "</p>";
+            }
+            messages.innerHTML +=
+            "<div class=\"message\">"
+                + headings + contents
+            "</div>";
+        }
     }
 }
