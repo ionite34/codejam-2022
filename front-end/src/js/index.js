63122d7e6832119be7706bcea03c81284e329bd8
apolloiheo <apolloiheo@berkeley.edu>
2022-07-30 19:09:15 -0700

    some more styling and added js to button

--- front-end/src/js/index.js
+++ front-end/src/js/index.js
@@ -1,1 +1,23 @@
 // empty
+
+let text_input = document.getElementById("user-message");
+text_input.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        InputEnter();
+    }
+})
+function InputEnter() {
+    console.log(text_input.value);
+    CreateMessage("user", text_input.value);
+    text_input.value = "";
+}
+
+function CreateMessage(from, content) {
+    if (from == "user") {
+        let messages = document.getElementsByClassName("messages")[0];
+        messages.innerHTML += 
+            "<div class=\"message\"> \
+                <p class=\"message-content\">" + content + "</p> \
+            </div>";
+    }
+}
